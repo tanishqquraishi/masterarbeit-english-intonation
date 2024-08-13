8f6b565bba1e78a2da7fd878976351c3df34d5bf
tanishqquraishi <qtanishq@gmail.com>
2024-08-13 17:03:02 +0200

    Added model to investigate likelihood of a monotonal PA per speaker group

--- data.py
+++ data.py
@@ -1,14 +1,1 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-
-# Rename the columns to more straightforward names
-data = data.rename(columns={
-    '1_anno_default_ns:bt': 'boundary_tone',
-    '1_meta_speaker-bilingual': 'bilingual',
-    '1_meta_setting': 'formality',
-    '1_meta_speaker-gender': 'gender',
-    '1_meta_speaker-id': 'speaker_id'
-})
+print("Move all data loading and cleaning to this module later and import it separately for every model")
