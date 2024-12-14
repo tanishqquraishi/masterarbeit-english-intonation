f8f1b6c478e6de82a1bfa60f81ea6c745df53be5
tanishqquraishi <qtanishq@gmail.com>
2024-12-14 14:38:31 +0100

    Added PA preprocessing and stats

---
+++ data_preparation/bt_preprocessing_stats.py
@@ -0,0 +1,114 @@
+import pandas as pd
+from preprocessing_utils import bt_merge_mappings , calculate_percentages, calculate_gender_percentages, apply_bt_merge_mappings, apply_bt_gender_merge_mappings, apply_bt_speaker_merge_mappings
+
+file_path = ""
+boundary_tones = pd.read_excel(file_path, sheet_name=1)  
+
+# Define the set of labels to discard
+discard_labels = {
+    "%H", "!H-", "L-%", "L-L", "-?%?", "L_", "L-(H)%", "(L)-%", "(L)-", "LHH%", "L-H*",
+    "L*+H", "(L)-(L)%", "L*", "!H-L", "HL-%", "^H*", "H_", "H-HL", "H-H", "-%", "!H*",
+    "L-L*", "L-L&"
+}
+
+# Discard labels
+boundary_tones_cleaned = boundary_tones[~boundary_tones["1_anno_default_ns:bt"].isin(discard_labels)]
+
+# Define the replacements as a list of tuples
+replacements = [
+    ("L-L%%", "L-L%"),
+    ("H-L%%", "H-L%"),
+    ("L-H%%", "L-H%"),
+    ("L-L5", "L-L%"),
+    ("H-H5", "H-H%"),
+    ("H-H%%", "H-H%")
+]
+
+# Apply the replacements
+for to_replace, value in replacements:
+    boundary_tones_cleaned = boundary_tones_cleaned.replace(to_replace=to_replace, value=value)
+
+boundary_tones_cleaned['1_anno_default_ns:bt'].value_counts()
+final_bt = pd.DataFrame(boundary_tones_cleaned)
+cleaned_file_path = "/content/data_17.07.2024.xlsx"
+bt_cleaned = pd.read_excel(cleaned_file_path, 1)
+
+
+def total_number_of_x(df, column_name):
+    return df[column_name].count()
+
+total_number_of_x(bt_cleaned, '1_anno_default_ns:bt')
+
+calculate_percentages(bt_cleaned, '1_anno_default_ns:bt', '1_meta_speaker-bilingual')
+
+#  % of male and female speakers out of total number of speakers for bt_cleaned
+
+# Calculate total number of speakers
+total_speakers = bt_cleaned['1_meta_speaker-id'].nunique()
+
+calculate_gender_percentages(bt_cleaned, '1_anno_default_ns:bt', '1_meta_speaker-gender')
+
+boundary_tones_all_counts = bt_cleaned['1_anno_default_ns:bt'].value_counts()
+apply_bt_merge_mappings(boundary_tones_all_counts, bt_merge_mappings)
+total_number_of_speakers = bt_cleaned['1_meta_speaker-id'].nunique()
+print(f"Total number of speakers: {total_number_of_speakers}")
+number_of_bilinguals = bt_cleaned[bt_cleaned['1_meta_speaker-bilingual'] == 'yes']['1_meta_speaker-id'].nunique()
+print(f"Number of bilingual speakers: {number_of_bilinguals}")
+number_of_monolinguals = bt_cleaned[bt_cleaned['1_meta_speaker-bilingual'] == 'no']['1_meta_speaker-id'].nunique()
+print(f"Number of monolingual speakers: {number_of_monolinguals}")
+bilingual_bt_count = bt_cleaned[bt_cleaned['1_meta_speaker-bilingual'] == 'yes']['1_anno_default_ns:bt'].value_counts()
+monolingual_bt_count = bt_cleaned[bt_cleaned['1_meta_speaker-bilingual'] == 'no']['1_anno_default_ns:bt'].value_counts()
+
+# Create dataframes for bilingual and monolingual counts
+bilingual_df = pd.DataFrame(bilingual_bt_count).reset_index()
+bilingual_df.columns = ['Boundary Tone', 'Bilingual Count']
+
+monolingual_df = pd.DataFrame(monolingual_bt_count).reset_index()
+monolingual_df.columns = ['Boundary Tone', 'Monolingual Count']
+
+speaker_group_bt = pd.merge(bilingual_df, monolingual_df, on='Boundary Tone', how='outer').fillna(0)
+
+# Ensure count columns are integers
+speaker_group_bt['Bilingual Count'] = speaker_group_bt['Bilingual Count'].astype(int)
+speaker_group_bt['Monolingual Count'] = speaker_group_bt['Monolingual Count'].astype(int)
+
+# Display the combined dataframe
+speaker_group_bt.sort_values(by=['Bilingual Count', 'Monolingual Count'], ascending=False).reset_index(drop=True)
+merged_speaker_group_bt= apply_bt_speaker_merge_mappings(speaker_group_bt, bt_merge_mappings)
+merged_speaker_group_bt = merged_speaker_group_bt.sort_values(by=['Bilingual Count', 'Monolingual Count'], ascending=False).reset_index(drop=True)
+merged_speaker_group_bt
+
+# prompt: get me number of male and female speakers from bt_cleaned
+
+# Calculate the number of male and female speakers
+number_of_male_speakers = bt_cleaned[bt_cleaned['1_meta_speaker-gender'] == 'male']['1_meta_speaker-id'].nunique()
+number_of_female_speakers = bt_cleaned[bt_cleaned['1_meta_speaker-gender'] == 'female']['1_meta_speaker-id'].nunique()
+
+print(f"Number of male speakers: {number_of_male_speakers}")
+print(f"Number of female speakers: {number_of_female_speakers}")
+
+
+# prompt: counts of 2_anno_default_ns:word_bt based on male and female in 1_meta_speaker-gender
+
+# Calculate pitch accent counts for male and female groups
+male_bt_count = bt_cleaned[bt_cleaned['1_meta_speaker-gender'] == 'male']['1_anno_default_ns:bt'].value_counts()
+female_bt_count = bt_cleaned[bt_cleaned['1_meta_speaker-gender'] == 'female']['1_anno_default_ns:bt'].value_counts()
+
+# Create DataFrames
+male_df = pd.DataFrame(male_bt_count).reset_index()
+male_df.columns = ['Boundary Tone', 'Male Count']
+
+female_df = pd.DataFrame(female_bt_count).reset_index()
+female_df.columns = ['Boundary Tone', 'Female Count']
+
+# Merge DataFrames
+gender_group_bt = pd.merge(male_df, female_df, on='Boundary Tone', how='outer').fillna(0)
+
+# Convert counts to integers
+gender_group_bt['Male Count'] = gender_group_bt['Male Count'].astype(int)
+gender_group_bt['Female Count'] = gender_group_bt['Female Count'].astype(int)
+
+gender_group_bt
+merged_gender_group_bt = apply_bt_gender_merge_mappings(gender_group_bt, bt_merge_mappings)
+merged_gender_group_bt = merged_gender_group_bt.sort_values(by=['Male Count', 'Female Count'], ascending=False).reset_index(drop=True)
+merged_gender_group_bt
---
+++ data_preparation/pa_preprocessing_stats.py
@@ -0,0 +1,166 @@
+import numpy as np
+import pandas as pd
+from preprocessing_utils import calculate_gender_percentages, calculate_percentages, pa_merge_mappings, apply_pa_gender_merge_mappings, apply_pa_merge_mappings, drop_diverse_gender
+
+"""# Files temporarily uploaded to run time"""
+
+file_path = "/content/pitch accents redone.xlsx"
+pitch_accents = pd.read_excel(file_path,) #Add sheet number along with file path
+
+pitch_accents_cleaned = pitch_accents[~pitch_accents['1_meta_speaker-id'].isin(["'NULL'"])]
+pitch_accents_cleaned.head(5)
+
+"""# Discard labels set"""
+
+pitch_accents_to_discard = ['L-L%', 'L**H', 'L-', 'H-L%', 'H-', '!H', 'L+', '*?', '*', 'L-H%', 'L*+^H*', '!H-L%', 'H-H%', 'H+L', 'L*+H*', 'L++H', 'L-H*', 'L*H*', 'L*+H%', '!H+', 'H+!H', 'L+H+']
+
+pitch_accents_cleaned['2_anno_default_ns:word_pa'] = pitch_accents_cleaned['2_anno_default_ns:word_pa'].replace(pitch_accents_to_discard, np.nan)
+
+# Correction labels set
+pa_replacements = [
+    ("H*,L-", "H*"),
+    ("L*,L*", "L*"),
+    ("L*,L*+H", "L*"),
+    ("H*,H-L%", "H*"),
+    ("L*,L-L%", "L*"),
+    ("H*,L-L%", "H*"),
+    ("L*,L-", "L*"),
+    ("^H*,H-L%", "^H*"),
+    ("L+H*, L-", "L+H*"),
+    ("!H*,L-L%", "!H*"),
+    ("H*,!H*", "H*"),
+    ("L+^H*,L-L%", "L+^H*"),
+    ("L*+H,H-", "L*+H"),
+    ("H*,H-", "H*"),
+    ("L*,H*", "L*"),
+    ("L*H", "L+*H"),
+    ("L*H+", "L*+H"),
+    ("HL*", "H+L*"),
+    ("H**", "H*"),
+    ("H*!H", "H*+!H"),
+    ("1H*", "H*"),
+    ("^H*^", "^H*"),
+    ("^H*,", "^H*"),
+    ("L+H*,L-", "L+H*")
+]
+
+for to_replace, value in pa_replacements:
+    pitch_accents_cleaned = pitch_accents_cleaned.replace(to_replace=to_replace, value=value)
+
+pa_cleaned = pitch_accents_cleaned
+
+total_pa_counts = pa_cleaned['2_anno_default_ns:word_pa'].value_counts()
+total_number_of_words_pa = pa_cleaned['1_anno_default_ns:norm'].count()
+print(f"Total number of words: {total_number_of_words_pa}")
+total_number_of_words_pa_bilingual = pa_cleaned.groupby('1_meta_speaker-bilingual')['1_anno_default_ns:norm'].count()
+print(f"Total number of words by bilingual: {total_number_of_words_pa_bilingual}")
+
+merged_pa_counts = apply_pa_merge_mappings(total_pa_counts, pa_merge_mappings)
+merged_pa_counts = merged_pa_counts.sort_values(ascending=False).reset_index(drop=False)
+merged_pa_counts.columns = ['2_anno_default_ns:word_pa', 'count']
+merged_pa_counts
+
+number_of_words = pa_cleaned['1_anno_default_ns:norm'].count()
+number_of_pa = pa_cleaned['2_anno_default_ns:word_pa'].count()
+average_pa = number_of_pa / number_of_words
+no_pa_percent= (number_of_words - number_of_pa) / number_of_words * 100
+
+print(f"Total number of words: {number_of_words}")
+print(f"Total number of Pitch Accents: {number_of_pa}")
+print(f"Average Number of Pitch Accents per word: {average_pa}")
+print(f"Percentage of words without Pitch Accents: {no_pa_percent:.2f}%")
+
+"""# Number of male and female speakers"""
+
+number_of_male_speakers = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'male']['1_meta_speaker-id'].nunique()
+number_of_female_speakers = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'female']['1_meta_speaker-id'].nunique()
+number_of_diverse_speakers = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'diverse']['1_meta_speaker-id'].nunique()
+
+print(f"Number of male speakers: {number_of_male_speakers}")
+print(f"Number of female speakers: {number_of_female_speakers}")
+print(f"Number of diverse speakers: {number_of_diverse_speakers}")
+pa_cleaned['1_meta_speaker-gender'].unique()
+
+"""# After dropping diverse speakers"""
+
+pa_cleaned = drop_diverse_gender(pa_cleaned)
+
+total_pa_counts = pa_cleaned['2_anno_default_ns:word_pa'].value_counts()
+total_number_of_words_pa = pa_cleaned['1_anno_default_ns:norm'].count()
+print(f"Total number of words: {total_number_of_words_pa}")
+total_number_of_words_pa_bilingual = pa_cleaned.groupby('1_meta_speaker-bilingual')['1_anno_default_ns:norm'].count()
+print(f"Total number of words by bilingual: {total_number_of_words_pa_bilingual}")
+
+merged_pa_counts = apply_pa_merge_mappings(total_pa_counts, pa_merge_mappings)
+merged_pa_counts = merged_pa_counts.sort_values(ascending=False).reset_index(drop=False)
+merged_pa_counts.columns = ['2_anno_default_ns:word_pa', 'count']
+merged_pa_counts
+
+number_of_words = pa_cleaned['1_anno_default_ns:norm'].count()
+number_of_pa = pa_cleaned['2_anno_default_ns:word_pa'].count()
+average_pa = number_of_pa / number_of_words
+no_pa_percent= (number_of_words - number_of_pa) / number_of_words * 100
+
+print(f"Total number of words: {number_of_words}")
+print(f"Total number of Pitch Accents: {number_of_pa}")
+print(f"Average Number of Pitch Accents per word: {average_pa}")
+print(f"Percentage of words without Pitch Accents: {no_pa_percent:.2f}%")
+
+number_of_male_speakers = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'male']['1_meta_speaker-id'].nunique()
+number_of_female_speakers = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'female']['1_meta_speaker-id'].nunique()
+
+print(f"Number of male speakers: {number_of_male_speakers}")
+print(f"Number of female speakers: {number_of_female_speakers}")
+
+"""# PA and Speaker Group"""
+
+# Calculate pitch accent counts for bilingual and monolingual groups
+bilingual_pa_count = pa_cleaned[pa_cleaned['1_meta_speaker-bilingual'] == 'yes']['2_anno_default_ns:word_pa'].value_counts()
+monolingual_pa_count = pa_cleaned[pa_cleaned['1_meta_speaker-bilingual'] == 'no']['2_anno_default_ns:word_pa'].value_counts()
+
+bilingual_df = pd.DataFrame(bilingual_pa_count).reset_index()
+bilingual_df.columns = ['Pitch Accent', 'Bilingual Count']
+
+monolingual_df = pd.DataFrame(monolingual_pa_count).reset_index()
+monolingual_df.columns = ['Pitch Accent', 'Monolingual Count']
+
+speaker_group_pa = pd.merge(bilingual_df, monolingual_df, on='Pitch Accent', how='outer').fillna(0)
+
+speaker_group_pa['Bilingual Count'] = speaker_group_pa['Bilingual Count'].astype(int)
+speaker_group_pa['Monolingual Count'] = speaker_group_pa['Monolingual Count'].astype(int)
+
+speaker_group_pa
+merged_speaker_group_pa= apply_pa_speaker_merge_mappings(speaker_group_pa, pa_merge_mappings)
+merged_speaker_group_pa = merged_speaker_group_pa.sort_values(by=['Bilingual Count', 'Monolingual Count'], ascending=False).reset_index(drop=True)
+merged_speaker_group_pa
+
+calculate_percentages(pa_cleaned, '2_anno_default_ns:word_pa', '1_meta_speaker-bilingual')
+
+"""# PA and Gender"""
+
+# Calculate pitch accent counts for male and female groups
+male_pa_count = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'male']['2_anno_default_ns:word_pa'].value_counts()
+female_pa_count = pa_cleaned[pa_cleaned['1_meta_speaker-gender'] == 'female']['2_anno_default_ns:word_pa'].value_counts()
+
+# Create DataFrames
+male_df = pd.DataFrame(male_pa_count).reset_index()
+male_df.columns = ['Pitch Accent', 'Male Count']
+
+female_df = pd.DataFrame(female_pa_count).reset_index()
+female_df.columns = ['Pitch Accent', 'Female Count']
+
+# Merge DataFrames
+gender_group_pa = pd.merge(male_df, female_df, on='Pitch Accent', how='outer').fillna(0)
+
+# Convert counts to integers
+gender_group_pa['Male Count'] = gender_group_pa['Male Count'].astype(int)
+gender_group_pa['Female Count'] = gender_group_pa['Female Count'].astype(int)
+
+gender_group_pa
+
+merged_gender_group_pa = apply_pa_gender_merge_mappings(gender_group_pa, pa_merge_mappings)
+merged_gender_group_pa = merged_gender_group_pa.sort_values(by=['Male Count', 'Female Count'], ascending=False).reset_index(drop=True)
+merged_gender_group_pa
+
+calculate_gender_percentages(pa_cleaned,  "2_anno_default_ns:word_pa", "1_meta_speaker-gender")
+
